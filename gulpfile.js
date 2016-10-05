55eb114a02e54356b7a1324b7c11ef25a2a02cd9
Brian Petersen <skiwalk3r@gmail.com>
2016-10-05 09:37:11 -0600

    changes dev workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,29 @@
 // Assigning modules to local variables
 var gulp = require('gulp');
+var del = require('del');
 var less = require('gulp-less');
 var browserSync = require('browser-sync').create();
-var header = require('gulp-header');
 var cleanCSS = require('gulp-clean-css');
 var rename = require("gulp-rename");
 var uglify = require('gulp-uglify');
-var pkg = require('./package.json');
-
-// Set the banner content
-var banner = ['/*!\n',
-    ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
-    ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-    ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n',
-    ' */\n',
-    ''
-].join('');
 
 // Default task
 gulp.task('default', ['less', 'minify-css', 'minify-js', 'copy']);
 
-// Less task to compile the less files and add the banner
+// Remove produced files
+gulp.task('clean', function() {
+    return del([
+        'css/styles.css',
+        'css/styles.min.css',
+        'js/**/*.min.js',
+        'vendor/**/*'
+    ]);
+});
+
+// Less task to compile the less files
 gulp.task('less', function() {
-    return gulp.src('less/freelancer.less')
+    return gulp.src('less/styles.less')
         .pipe(less())
-        .pipe(header(banner, { pkg: pkg }))
         .pipe(gulp.dest('css'))
         .pipe(browserSync.reload({
             stream: true
@@ -33,7 +32,7 @@ gulp.task('less', function() {
 
 // Minify CSS
 gulp.task('minify-css', function() {
-    return gulp.src('css/freelancer.css')
+    return gulp.src('css/styles.css')
         .pipe(cleanCSS({ compatibility: 'ie8' }))
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('css'))
@@ -44,9 +43,8 @@ gulp.task('minify-css', function() {
 
 // Minify JS
 gulp.task('minify-js', function() {
-    return gulp.src('js/freelancer.js')
+    return gulp.src(['js/**/*.js', '!**/*.min.js'])
         .pipe(uglify())
-        .pipe(header(banner, { pkg: pkg }))
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('js'))
         .pipe(browserSync.reload({
@@ -96,7 +94,7 @@ gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js'], function() 
     gulp.watch('less/*.less', ['less']);
     gulp.watch('css/*.css', ['minify-css']);
     gulp.watch('js/*.js', ['minify-js']);
+
     // Reloads the browser whenever HTML or JS files change
     gulp.watch('*.html', browserSync.reload);
-    gulp.watch('js/**/*.js', browserSync.reload);
 });
